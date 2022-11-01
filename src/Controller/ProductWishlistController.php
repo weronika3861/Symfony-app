4e83c84457c7bfadc2e9191430ef88cd390eaf5f
weronika3861 <weronikami@onet.pl>
2022-11-01 19:54:36 +0100

    Add removing products from wishlist

--- src/Controller/ProductWishlistController.php
+++ src/Controller/ProductWishlistController.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Exception\EmptyWishlistException;
 use App\Exception\ProductAlreadyInWishlistException;
+use App\Exception\ProductLimitExceededException;
+use App\Exception\ProductNotExistException;
+use App\Exception\WishlistNotContainProductException;
 use App\Service\ProductWishlistService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,10 +44,46 @@ class ProductWishlistController extends AbstractController
             $this->productWishlistService->addByProductId($id);
 
             $this->addFlash('success', 'Product has been added to wishlist.');
-        } catch (ProductAlreadyInWishlistException $exception) {
+        } catch (ProductAlreadyInWishlistException | ProductLimitExceededException $exception) {
             $this->addFlash('error', $exception->getMessage());
         } catch (\Exception $exception) {
             $this->addFlash('error', 'Adding to wishlist was not successful. Try again.');
+        }
+
+        return $this->redirectToRoute('wishlist_index');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="wishlist_delete_product", methods={"POST"})
+     */
+    public function deleteProduct(int $id): Response
+    {
+        try {
+            $this->productWishlistService->deleteByProductId($id);
+
+            $this->addFlash('success', 'Removing product was successful.');
+        } catch (ProductNotExistException | WishlistNotContainProductException | EmptyWishlistException $exception) {
+            $this->addFlash('error', $exception->getMessage());
+        } catch (\Exception $exception) {
+            $this->addFlash('error', 'Removing product was not successful. Try again.');
+        }
+
+        return $this->redirectToRoute('wishlist_index');
+    }
+
+    /**
+     * @Route("/delete", name="wishlist_delete_all_products", methods={"POST"})
+     */
+    public function deleteAllProducts(): Response
+    {
+        try {
+            $this->productWishlistService->deleteAllProductIds();
+
+            $this->addFlash('success', 'Removing all products was successful.');
+        } catch (EmptyWishlistException $exception) {
+            $this->addFlash('error', $exception->getMessage());
+        } catch (\Exception $exception) {
+            $this->addFlash('error', 'Removing all products was not successful. Try again.');
         }
 
         return $this->redirectToRoute('wishlist_index');
